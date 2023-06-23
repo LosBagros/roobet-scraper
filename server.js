92b8f5c4b99a9db439f5aabe8a80e80a6e0ef04c
Bagros <45005533+LosBagros@users.noreply.github.com>
2023-06-23 18:14:16 +0300

    reconnect ws

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 const WebSocket = require('ws');
-const socket = new WebSocket('wss://api.roobet.party/socket.io/?EIO=3&transport=websocket'); // Replace with your server's URL
 
 let pool;
 
@@ -16,9 +15,12 @@ async function initialize() {
         connectionLimit: 10,
         queueLimit: 0
     });
+    connectSocket();
 }
 
 initialize();
+function connectSocket() {
+  const socket = new WebSocket('wss://api.roobet.party/socket.io/?EIO=3&transport=websocket'); // Replace with your server's URL
 
 socket.addEventListener('open', function(event) {
   console.log('Connected to the WebSocket server.');  
@@ -43,47 +45,48 @@ setInterval(() => {
 
 async function saveData(data) {
   const connection = await pool.getConnection();
-
-  try {
-      const {
-          _id, betAmount, balanceType, currency, closedOut, closeoutComplete, paidOut,
-          ranHooks, attempts, betId, gameName, gameNameDisplay, transactionIds,
-          thirdParty, category, gameIdentifier, payoutValue, mult, profit,
-          gameSessionId, userId, won, timestamp, closeoutTimestamp,
-          username, twoFactor, ...otherData
-      } = data;
-
-      // Handle user data
-      const userSQL = `
-          INSERT INTO users (id, name, twoFactor) VALUES (?, ?, ?)
-          ON DUPLICATE KEY UPDATE name = VALUES(name), twoFactor = VALUES(twoFactor);
-      `;
-      const userParams = [userId || null, username || null, twoFactor || null];
-      await connection.query(userSQL, userParams);
-
-      // Handle bet data
-      const betSQL = `
-          INSERT INTO bet (_id, betAmount, balanceType, currency, closedOut, closeoutComplete,
-          paidOut, ranHooks, attempts, betId, gameName, gameNameDisplay, transactionIds,
-          thirdParty, category, gameIdentifier, payoutValue, mult, profit,
-          gameSessionId, userId, won)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-      `;
-      const betParams = [
-          _id || null, betAmount || null, balanceType || null, currency || null,
-          closedOut || null, closeoutComplete || null, paidOut || null, ranHooks || null,
-          attempts || null, betId || null, gameName || null, gameNameDisplay || null,
-          transactionIds ? JSON.stringify(transactionIds) : null, thirdParty || null,
-          category || null, gameIdentifier || null, payoutValue || null, mult || null,
-          profit || null, gameSessionId || null, userId || null, won || null
-      ];
-      await connection.query(betSQL, betParams);
-
-      // console.log(`Saved data for user: ${userId}`);
-
-  } catch (error) {
-      console.error(`Failed to insert data: ${error}`);
-  } finally {
-      connection.release();
+  
+    try {
+        const {
+            _id, betAmount, balanceType, currency, closedOut, closeoutComplete, paidOut,
+            ranHooks, attempts, betId, gameName, gameNameDisplay, transactionIds,
+            thirdParty, category, gameIdentifier, payoutValue, mult, profit,
+            gameSessionId, userId, won, timestamp, closeoutTimestamp,
+            username, twoFactor, ...otherData
+        } = data;
+      
+        // Handle user data
+        const userSQL = `
+            INSERT INTO users (id, name, twoFactor) VALUES (?, ?, ?)
+            ON DUPLICATE KEY UPDATE name = VALUES(name), twoFactor = VALUES(twoFactor);
+        `;
+        const userParams = [userId || null, username || null, twoFactor || null];
+        await connection.query(userSQL, userParams);
+      
+        // Handle bet data
+        const betSQL = `
+            INSERT INTO bet (_id, betAmount, balanceType, currency, closedOut, closeoutComplete,
+            paidOut, ranHooks, attempts, betId, gameName, gameNameDisplay, transactionIds,
+            thirdParty, category, gameIdentifier, payoutValue, mult, profit,
+            gameSessionId, userId, won)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `;
+        const betParams = [
+            _id || null, betAmount || null, balanceType || null, currency || null,
+            closedOut || null, closeoutComplete || null, paidOut || null, ranHooks || null,
+            attempts || null, betId || null, gameName || null, gameNameDisplay || null,
+            transactionIds ? JSON.stringify(transactionIds) : null, thirdParty || null,
+            category || null, gameIdentifier || null, payoutValue || null, mult || null,
+            profit || null, gameSessionId || null, userId || null, won || null
+        ];
+        await connection.query(betSQL, betParams);
+      
+        // console.log(`Saved data for user: ${userId}`);
+      
+    } catch (error) {
+        console.error(`Failed to insert data: ${error}`);
+    } finally {
+        connection.release();
+    }
   }
 }
